420d525f252662b5ce1c523f1d36604fe499e30f
Shawn Dsilva <shawn@shawndsilva.com>
2021-10-31 19:30:10 +0530

    add repo button css done

--- src/components/SelectRepository.js
+++ src/components/SelectRepository.js
@@ -3,7 +3,9 @@ import React from 'react'
 function SelectRepository() {
     return (
         <div className='select-repo'>
-            
+            <div className='add-repo-container'>
+                <button className='add-repo-button'> + </button>
+            </div>
         </div>
     )
 }
